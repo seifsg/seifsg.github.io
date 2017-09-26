70658f6d6e6dc8f2e2348dd78b890e3d740b432c
Seif Sgayer <seifsgayer@gmail.com>
2017-09-26 06:13:43 +0100

    Checkpoint

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,63 @@
 var selectedTab;
 
 var sections = {
-    actualitiesSection:{
+    actualitiesSection: {
         visible: true,
-        el: $("#actualitiesSection")
+        el: $("#actualitiesSection"),
+        nav: $("[data-sec='actualitiesSection']").parent()
     },
-    servicesSection:{
+    servicesSection: {
         visible: false,
-        el: $("#servicesSection")
+        el: $("#servicesSection"),
+        nav: $("[data-sec='servicesSection']").parent()
     },
     storySection: {
         visible: false,
-        el: $("#storySection")
+        el: $("#storySection"),
+        nav: $("[data-sec='storySection']").parent()
     },
     usefulSection: {
         visible: false,
-        el: $("#usefulSection")
+        el: $("#usefulSection"),
+        nav: $("[data-sec='usefulSection']").parent()
+    },
+    contactSection: {
+        visible: false,
+        el: $("#contactSection"),
+        nav: $("[data-sec='contactSection']").parent()
     }
 }
 
-$(".nav-link").on('click',function(e){
+$(".nav-link").on('click', function (e) {
     var section = $(this).attr('data-sec');
-    console.log(section == 'undefined');
-    if(section == undefined) return true;
+    if (section == undefined) return true;
 
     e.preventDefault();
 
-    
-
-    for(var sec in sections){
-        if(sec == section){
+    for (var sec in sections) {
+        if (sec == section) {
             sections[sec].visible = true;
             sections[sec].el.show();
-        }else{
+            sections[sec].nav.addClass('active');
+        } else {
             sections[sec].visible = false;
             sections[sec].el.hide();
+            sections[sec].nav.removeClass('active');
         }
     }
 
-})
+});
+
+
+var randomPhrase = ()=>{
+
+    var phrases = ['Not so regular website','Be different','Always keep improving'];
+    $("#phrases").text(phrases[Math.floor(Math.random()*3)]);
+
+}
+
+$(document).ready(()=>{
+    $('[data-toggle="tooltip"]').tooltip();
+    randomPhrase();
+});
+
