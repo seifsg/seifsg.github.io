dcca0c156e2db77d8771e11db25dcea4c67cfe5b
Seif Sgayer <seifsgayer@gmail.com>
2017-10-15 08:37:34 +0100

    putting the coming soon msg

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,36 @@
-var selectedTab;
+var sectionsNames = ['actualitiesSection', 'servicesSection', 'bioSection', 'usefulSection', 'cvSection'];
 
-var sections = {
-    actualitiesSection: {
-        visible: true,
-        el: $("#actualitiesSection"),
-        nav: $("[data-sec='actualitiesSection']").parent()
-    },
-    servicesSection: {
+//just dynamic assignments
+var sections = {};
+sectionsNames.map(section => {
+    var nav = $(`[data-sec='${section}']`);
+
+    sections[section] = {
         visible: false,
-        el: $("#servicesSection"),
-        nav: $("[data-sec='servicesSection']").parent()
-    },
-    storySection: {
-        visible: false,
-        el: $("#storySection"),
-        nav: $("[data-sec='storySection']").parent()
-    },
-    usefulSection: {
-        visible: false,
-        el: $("#usefulSection"),
-        nav: $("[data-sec='usefulSection']").parent()
-    },
-    contactSection: {
-        visible: false,
-        el: $("#contactSection"),
-        nav: $("[data-sec='contactSection']").parent()
-    }
-}
+        el: $(`#${section}`),
+        navA: nav,
+        navLi: nav.parent(),
+        title: ''
+    };
+});
+
+//seeding required information
+var pageTitleInital = 'SeifSG - Seif Sgayer - ';
+sections[sectionsNames[0]].visible = true;
+sections[sectionsNames[0]].title = 'Hire a Freelancer &amp; Learn from a Computer Scientist/Developer/Coder';
+sections[sectionsNames[1]].title = 'Freelancer | FullStack Web Development | Software Engineering';
+sections[sectionsNames[2]].title = 'Biography';
+sections[sectionsNames[3]].title = 'Useful links and resources';
+sections[sectionsNames[4]].title = 'Curriculum Vitae (CV)';
+
+
 
 $(".nav-link").on('click', function (e) {
+
+    // for now it does nothing.
+    e.preventDefault();
+    return;
+
     var section = $(this).attr('data-sec');
     if (section == undefined) return true;
 
@@ -38,26 +40,31 @@ $(".nav-link").on('click', function (e) {
         if (sec == section) {
             sections[sec].visible = true;
             sections[sec].el.show();
-            sections[sec].nav.addClass('active');
+            sections[sec].navLi.addClass('active');
+            changeTitle(sec);
+            randomPhrase();
         } else {
             sections[sec].visible = false;
             sections[sec].el.hide();
-            sections[sec].nav.removeClass('active');
+            sections[sec].navLi.removeClass('active');
         }
     }
 
 });
 
 
-var randomPhrase = ()=>{
+var randomPhrase = () => {
 
-    var phrases = ['Not so regular website','Be different','Always keep improving'];
-    $("#phrases").text(phrases[Math.floor(Math.random()*3)]);
+    var phrases = ['Not so regular website', 'Be different', 'Always keep improving'];
+    $("#phrases").text(phrases[Math.floor(Math.random() * 3)]);
 
 }
 
-$(document).ready(()=>{
+var changeTitle = sec =>{
+    $('title').text(pageTitleInital + sections[sec].title);
+}
+
+$(document).ready(() => {
     $('[data-toggle="tooltip"]').tooltip();
     randomPhrase();
 });
-
