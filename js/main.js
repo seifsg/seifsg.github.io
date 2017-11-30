3fa3991eba20e9eaa146b34f0dfdd31f3530b535
Seif Sgayer <seifsgayer@gmail.com>
2017-11-30 19:48:40 +0100

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-
-
-$(document).ready(function(){
+$(document).ready(function () {
 
     $(".button-collapse").sideNav({
-        menuWidth: 190, // Default is 240
-        edge: 'left', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        menuWidth: 190,
+        edge: 'left',
+        closeOnClick: true
     });
 
-    $("#love").addClass('underlined');
-    $("#bigHeart").addClass('red-text').css('text-shadow','1px 1px 5px #fff');
+    $("#bigHeart").addClass('red-text').css('text-shadow', '1px 1px 5px #fff');
 
 });
