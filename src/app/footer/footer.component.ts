7a33e4123d5b20d7d9e504c296396c3db9b1aa11
Seif Sgayer <seifsgayer@gmail.com>
2017-12-20 10:42:39 +0100

    first footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  year: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.year = new Date().getFullYear();
   }
 
 }
