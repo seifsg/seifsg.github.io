94ed039b4829fb6566a49c038870bf2b5ebf60cd
Seif Sgayer <seifsgayer@gmail.com>
2018-01-05 17:08:48 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
-var gulp = require('gulp');
-var parcel = require('gulp-parcel');
+"use strict";
 
-gulp.task('html', function () {
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var parcel = require('gulp-parcel');
+var browserSync = require('browser-sync').create();
+var run = require('gulp-run-command').default;
+var log = require('fancy-log');
+var errorHandler = require('gulp-error-handle');
+
+/*gulp.task('html', function () {
 	return gulp.src('src/templates/*.pug')
-		.pipe(parcel())
-		.pipe(gulp.dest('.'))
+		.pipe(pug())
+		.pipe(gulp.dest('build/html'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});*/
+
+
+gulp.task('browserSync', function () {
+	browserSync.init({
+		server: {
+			baseDir: 'dist'
+		},
+	});
 });
 
-gulp.task('default', ['html']);
+gulp.task('watch', gulp.parallel('browserSync'), function () {
+	gulp.watch('dist/**/*',browserSync.reload);
+});
+
+gulp.task('default', gulp.series('watch'), function () {
+	console.log('Started default');
+});
