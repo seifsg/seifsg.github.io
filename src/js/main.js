2f0faacbba28054adfc5afa0687478b4f3260eae
Seif Sgayer <seifsgayer@gmail.com>
2018-01-09 17:18:51 +0100

    nearly done sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -4,20 +4,24 @@ import Typed from './typed';
 const times = $("#times");
 const sidebar = $("#sidenav"); //who cares how it is named
 const main = $("main");
+const hireMe = $("#hireme");
 
 times.on('click', toggleSideBar);
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
     var typed = new Typed(".hero-text", {
         strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
         typeSpeed: 100,
         backSpeed: 30,
         onBegin: (self) => { // i made that :)
-          console.log('Hello there');
-          console.log(self);
+            console.log('Hello there');
+            console.log(self);
         }
-      });
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
 
     scaleVideoContainer();
 
@@ -25,7 +29,7 @@ $( document ).ready(function() {
     initBannerVideoSize('.video-container .filter');
     initBannerVideoSize('.video-container video');
 
-    $(window).on('resize', function() {
+    $(window).on('resize', function () {
         scaleVideoContainer();
         scaleBannerVideoSize('.video-container .poster img');
         scaleBannerVideoSize('.video-container .filter');
@@ -38,13 +42,13 @@ function scaleVideoContainer() {
 
     var height = $(window).height() + 5;
     var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height',unitHeight);
+    $('.homepage-hero-module').css('height', unitHeight);
 
 }
 
-function initBannerVideoSize(element){
+function initBannerVideoSize(element) {
 
-    $(element).each(function(){
+    $(element).each(function () {
         $(this).data('height', $(this).height());
         $(this).data('width', $(this).width());
     });
@@ -53,24 +57,27 @@ function initBannerVideoSize(element){
 
 }
 
-function scaleBannerVideoSize(element){
+function scaleBannerVideoSize(element) {
 
     var windowWidth = $(window).width(),
-    windowHeight = $(window).height() + 5,
-    videoWidth,
-    videoHeight;
+        windowHeight = $(window).height() + 5,
+        videoWidth,
+        videoHeight;
 
     // console.log(windowHeight);
 
-    $(element).each(function(){
-        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+    $(element).each(function () {
+        var videoAspectRatio = $(this).data('height') / $(this).data('width');
 
         $(this).width(windowWidth);
 
-        if(windowWidth < 1000){
+        if (windowWidth < 1000) {
             videoHeight = windowHeight;
             videoWidth = videoHeight / videoAspectRatio;
-            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+            $(this).css({
+                'margin-top': 0,
+                'margin-left': -(videoWidth - windowWidth) / 2 + 'px'
+            });
 
             $(this).width(videoWidth).height(videoHeight);
         }
@@ -80,23 +87,32 @@ function scaleBannerVideoSize(element){
     });
 }
 
-function toggleSideBar(){
+function toggleSideBar() {
 
     console.log('hi');
-    if( sidebar.attr('data-state') == undefined ){
-        sidebar.attr('data-state','closed');
+    if (sidebar.attr('data-state') == undefined) {
+        sidebar.attr('data-state', 'closed');
     }
-    
-    if(sidebar.attr('data-state') == 'closed'){
-        sidebar.css('margin-left','0');
-        main.css('padding-left','240px');
 
-        sidebar.attr('data-state','open');
-    }else{
-        sidebar.css('margin-left','-240px');
-        main.css('padding-left','0');
+    if (sidebar.attr('data-state') == 'closed') {
+        // opening
+        sidebar.css('margin-left', '0');
+        main.css('padding-left', '240px');
 
-        sidebar.attr('data-state','closed');
+        // dat jucy hire me action
+        hireMe.css('color', "#fff");
+        setTimeout(() => hireMe.addClass('animated flash'), 1000);
+        setTimeout(() => hireMe.css('color', "#ccc"), 1500);
+
+        sidebar.attr('data-state', 'open');
+    } else {
+        //closing
+        sidebar.css('margin-left', '-240px');
+        main.css('padding-left', '0');
+
+        hireMe.removeClass('animated flash');
+
+        sidebar.attr('data-state', 'closed');
     }
 
 }
